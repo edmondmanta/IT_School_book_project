2e2bb8fd3b478fd27414a91b8ea75ffac950d515
edmondmanta <edmondmanta@yahoo.com>
2021-04-13 20:31:58 +0300

    Main menu config and empty user functions

---
+++ ItSchoolBookApp.py
@@ -0,0 +1,30 @@
+#
+def add_book():
+    print("Add a book option")
+def list_book():
+    print("List books option")
+def update_book():
+    print("Update a book option")
+def share_book():
+    print("Share a book option")
+
+
+# Main menu for user
+print('Menu: ')
+print('1: Add a book')
+print('2: List books')
+print('3: Update a book')
+print('4: Share a book')
+
+option = int(input("Select one option -> "))
+
+if option == 1:
+    add_book()
+elif option == 2:
+    list_book()
+elif option == 3:
+    update_book()
+elif option == 4:
+    share_book()
+else:
+    print("Not a valid option")
