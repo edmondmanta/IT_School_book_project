63a5782786b14dcd759917e25d9eddc7143d8660
edmondmanta <edmondmanta@yahoo.com>
2021-04-27 20:03:15 +0300

    initial implementation of update books

--- ItSchoolBookApp.py
+++ ItSchoolBookApp.py
@@ -37,7 +37,29 @@ def list_book():
             # print(f"The book is read: {row.get('IsRead',False)}")
 
 def update_book():
-    print("Update a book option")
+    book_name = input("Enter book name: ")
+    book_read = input("Is the book read?(Y/N)? ")
+    if book_read == 'Y':
+        book_read = True
+    else:
+        book_read = False
+    import csv
+    rows = []
+    with open('booksDB.csv', mode='rw') as file:
+        # rows = list(csv.DictReader(file))
+        rows = csv.DictReader(file, fieldnames=[
+            'BookName', 'AuthorName', 'SharedWith', 'IsRead'
+        ])
+        for row in rows:
+            if row.get('BookName') == book_name:
+                row['IsRead'] = book_read
+                csv_writer = csv.DictWriter(file, fieldnames=[
+                    'BookName', 'AuthorName', 'SharedWith', 'IsRead'
+                ])
+                csv_writer.writerows(rows)
+                break
+        print('Book was updated succefully ')
+
 def share_book():
     print("Share a book option")
 
